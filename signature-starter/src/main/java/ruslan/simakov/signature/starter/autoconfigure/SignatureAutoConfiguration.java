cb2396f301ab9089e0d037ea2bb983549e7a34f7
Ruslan Simakov <ua667766706@gmail.com>
2021-09-14 12:49:04 +0200

    added signature service enable/disable

--- signature-starter/src/main/java/ruslan/simakov/signature/starter/autoconfigure/SignatureAutoConfiguration.java
+++ signature-starter/src/main/java/ruslan/simakov/signature/starter/autoconfigure/SignatureAutoConfiguration.java
@@ -1,7 +1,10 @@
 package ruslan.simakov.signature.starter.autoconfigure;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("ruslan.simakov.signature.starter")
+@ConditionalOnProperty(value = "signature.enable", havingValue = "true", matchIfMissing = true)
+//could switch on property, ifTrue or doesn't exist - would be switch on
 public class SignatureAutoConfiguration {
 }
--- signature-starter/src/main/java/ruslan/simakov/signature/starter/controller/SignatureController.java
+++ signature-starter/src/main/java/ruslan/simakov/signature/starter/controller/SignatureController.java
@@ -1,10 +1,26 @@
 package ruslan.simakov.signature.starter.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.Duration;
 
 @RestController
 public class SignatureController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SignatureController.class);
+
+    private final String username;
+    private final Duration retentionPeriod;
+
+    SignatureController(@Value("${signature.username:NONAME}") String username,
+                        @Value("${signature.extra.retention-period:5m}") Duration retentionPeriod){
+        this.username = username;
+        this.retentionPeriod = retentionPeriod;
+        LOG.info("Using following configuration: username - {}, retention period - {}", username, retentionPeriod);
+    }
 
     @GetMapping("signature")
     public String getSignature() {
