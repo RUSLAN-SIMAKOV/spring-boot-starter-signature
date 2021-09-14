9b6532fbfb9c99d2e405ba9c9e1d461db9ed9e54
Ruslan Simakov <ua667766706@gmail.com>
2021-09-14 13:48:23 +0200

    added hibernate validator

--- signature-starter/src/main/java/ruslan/simakov/signature/starter/autoconfigure/SignatureProperties.java
+++ signature-starter/src/main/java/ruslan/simakov/signature/starter/autoconfigure/SignatureProperties.java
@@ -1,12 +1,24 @@
 package ruslan.simakov.signature.starter.autoconfigure;
 
+import org.hibernate.validator.constraints.time.DurationMax;
+import org.hibernate.validator.constraints.time.DurationMin;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.Duration;
 
+@Validated
 @ConfigurationProperties("signature")
 public class SignatureProperties {
 
+
+    /**
+     * Username for email signature
+     */
+    @NotBlank
+    @Size(min = 5, max = 6)
     private String username = "NONAME";
     private Extra extra = new Extra();
 
@@ -28,6 +40,11 @@ public class SignatureProperties {
 
     public static class Extra {
 
+        /**
+         * Retention period for email
+         */
+        @DurationMin(minutes = 10)
+        @DurationMax(hours = 9)
         private Duration retentionPeriod = Duration.ofMinutes(5);
 
         public Duration getRetentionPeriod() {
